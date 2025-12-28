853dc4c7a7b81dbf32b2d38e6ab5a44a27ea0b9a
couvq <quentincouvelaire@gmail.com>
2025-12-28 14:05:21 -0500

    initial script for pulling listing data

--- extension/pull_zillow_listing.ts
+++ extension/pull_zillow_listing.ts
@@ -1,1 +1,21 @@
-alert('deal analyzer triggered!')
+interface ListingData {
+  zillowHomeDetailsLink: string;
+  price: number;
+}
+
+const zillowHomeDetailsLink = document.URL;
+const price = Number(
+  document
+    .querySelector('[data-testid="price"] > span')
+    .textContent.replace(/[$,:]/g, "")
+);
+
+const listingData: ListingData = {
+  zillowHomeDetailsLink,
+  price,
+};
+
+const baseUrl = "http://localhost:5173";
+const redirectUrl = `${baseUrl}?zillowHomeDetailsLink=${listingData.zillowHomeDetailsLink}&price=${listingData.price}`;
+
+alert(redirectUrl);
