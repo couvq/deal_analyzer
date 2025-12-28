e28916c0231458112cdc6fbed2676ddc7e60feb0
couvq <quentincouvelaire@gmail.com>
2025-12-28 14:31:24 -0500

    basic button with vanilla js

--- extension/pull_zillow_listing.ts
+++ extension/pull_zillow_listing.ts
@@ -3,6 +3,7 @@ interface ListingData {
   price: number;
 }
 
+// scrape relevant listing data
 const zillowHomeDetailsLink = document.URL;
 const price = Number(
   document
@@ -18,4 +19,15 @@ const listingData: ListingData = {
 const baseUrl = "http://localhost:5173";
 const redirectUrl = `${baseUrl}?zillowHomeDetailsLink=${listingData.zillowHomeDetailsLink}&price=${listingData.price}`;
 
-alert(redirectUrl);
+// render a button to redirect to the deal analyzer webpage with the redirect url we have constructed
+// TODO - figure out ts issue so that you can render a react component instead
+const extensionBtn = document.createElement("button");
+const extensionBtnLabel = document.createTextNode("Analyze deal");
+extensionBtn.appendChild(extensionBtnLabel);
+extensionBtn.setAttribute("id", "deal_analyzer_trigger");
+extensionBtn.style.position = "fixed";
+extensionBtn.style.right = "50px";
+extensionBtn.style.top = "50px";
+extensionBtn.style.zIndex = "999999999";
+extensionBtn.onclick = () => window.open(redirectUrl, "_blank");
+document.body.appendChild(extensionBtn);
