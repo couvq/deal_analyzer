9f322ef3f5f49e0152ccf1cce5393bf5d633c08f
couvq <quentincouvelaire@gmail.com>
2026-01-01 07:27:48 -0500

    refactor ui to use tabs

--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -3,16 +3,27 @@ import type { FormValues } from "@/types/shared";
 import { useForm } from "react-hook-form";
 import PropertyInfo from "./forms/PropertyInfo";
 import Analysis from "./Analysis";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 
 const MultiStepForm = () => {
   const { register, control } = useForm<FormValues>({
     defaultValues: getDefaultValuesFromUrl(),
   });
-  
+
   return (
     <>
-      <PropertyInfo register={register} />
-      <Analysis control={control} />
+      <Tabs defaultValue="propertyInfo">
+        <TabsList>
+          <TabsTrigger value="propertyInfo">Property</TabsTrigger>
+          <TabsTrigger value="analysis">Analysis</TabsTrigger>
+        </TabsList>
+        <TabsContent value="propertyInfo">
+          <PropertyInfo register={register} />
+        </TabsContent>
+        <TabsContent value="analysis">
+          <Analysis control={control} />
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
